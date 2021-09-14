aa89e25d5a6e23b04826b41a768d3d3d35cd3fcb
VanillaCold <35960636+VanillaCold@users.noreply.github.com>
2021-09-14 13:17:45 +0100

    improvements to custom SPUI element

--- EmpireInfo+/EmpireInfo.cpp
+++ EmpireInfo+/EmpireInfo.cpp
@@ -42,37 +42,38 @@ void EmpireInfo::Update()
 		win->FindWindowByID(0x03754E6C)->SetControlID(id("EmpireInfo"));
 	}
 	
-	if ( WindowManager.GetMainWindow()->FindWindowByID(id("EmpireInfo")) && WindowManager.GetMainWindow()->FindWindowByID(0x034C963F))
+	if (WindowManager.GetMainWindow()->FindWindowByID(id("EmpireInfo")) && WindowManager.GetMainWindow()->FindWindowByID(0x034C963F))
 	{
+		UTFWin::IWindow* empInfoWindow = WindowManager.GetMainWindow()->FindWindowByID(id("EmpireInfo"));
 		string16 colonyString;
 		colonyString.assign_convert(to_string(totalSystemsColonized));
 		colonyString = u"Total colonies: " + colonyString;
-		win->FindWindowByID(id("EmpireInfo"))->SetCaption(colonyString.c_str());
-	
-	auto colonies = Simulator::SpacePlayerData::Get()->mPlayerColonies.size();
-	if (totalSystemsColonized != 0)
-	{
-		if (totalSystemsColonized < colonies)
-		{
-			systemsColonized -= totalSystemsColonized - colonies;
-			
-		}
-	}
+		empInfoWindow->SetCaption(colonyString.c_str());
 
-	//App::ConsolePrintF(to_string(systemsColonized).c_str());
-	//App::ConsolePrintF(to_string(totalSystemsColonized).c_str());
-	auto anchorWindow = WindowManager.GetMainWindow()->FindWindowByID(0x034C963F);
-	//auto anchorArea = anchorWindow->GetRealArea();
-	win->SetParentWindow(anchorWindow);
-	totalSystemsColonized = Simulator::SpacePlayerData::Get()->mPlayerColonies.size();
-	string16 newColonyString;
-	newColonyString.assign_convert(to_string(systemsColonized));
-	newColonyString = u"\n New colonies: " + newColonyString;
-	colonyString += newColonyString;
-	win->FindWindowByID(id("EmpireInfo"))->SetCaption(colonyString.c_str());
-	win->FindWindowByID(id("EmpireInfo"))->SetArea(Math::Rectangle(0,  - 25, 230, 25));
-	anchorWindow->SetFlag(UTFWin::WindowFlags::kWinFlagClip, false);
-	//SporeDebugPrint("%i", 10 + 5 * colonyString.length());
+		auto colonies = Simulator::SpacePlayerData::Get()->mPlayerColonies.size();
+		if (totalSystemsColonized != 0)
+		{
+			if (totalSystemsColonized < colonies)
+			{
+				systemsColonized -= totalSystemsColonized - colonies;
+
+			}
+		}
+
+		//App::ConsolePrintF(to_string(systemsColonized).c_str());
+		//App::ConsolePrintF(to_string(totalSystemsColonized).c_str());
+		auto anchorWindow = WindowManager.GetMainWindow()->FindWindowByID(0x034C963F);
+		//auto anchorArea = anchorWindow->GetRealArea();
+		win->SetParentWindow(anchorWindow);
+		totalSystemsColonized = Simulator::SpacePlayerData::Get()->mPlayerColonies.size();
+		string16 newColonyString;
+		newColonyString.assign_convert(to_string(systemsColonized));
+		newColonyString = u"\nNew colonies: " + newColonyString;
+		colonyString += newColonyString;
+		empInfoWindow->SetCaption(colonyString.c_str());
+		empInfoWindow->SetArea(Math::Rectangle(25, -25, 220, 25));
+		anchorWindow->SetFlag(UTFWin::WindowFlags::kWinFlagClip, false);
+		//SporeDebugPrint("%i", 10 + 5 * colonyString.length());
 
 	}
 }
